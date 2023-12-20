299c4191cf4f14f048f308d0f529b4b0db96ec43
AlekseiEvgenievich <aleksei.smirnov@phystech.edu>
2023-12-20 18:14:29 +0100

    change the name of dict

--- sjapi.py
+++ sjapi.py
@@ -44,9 +44,9 @@ def predict_rub_salary_hh(vacancy):
             return None
 
 
-def get_hh_dict(hh_search_field,hh_area,hh_period):
+def get_hh_job_data(hh_search_field,hh_area,hh_period):
     languages = ['Python', 'Java', 'C++', 'JavaScript','C#','C','ruby','go', '1c', 'PHP','Shell', 'Scala', 'Swift']
-    hh_dict = {lang: {"vacancies_found": 0, "vacancies_processed": 0, "average_salary": 0} for lang in languages}
+    hh_job_data = {lang: {"vacancies_found": 0, "vacancies_processed": 0, "average_salary": 0} for lang in languages}
     hh_url = 'https://api.hh.ru/vacancies/'
     for language in languages:
         url = 'https://api.hh.ru/vacancies/'
@@ -56,24 +56,25 @@ def get_hh_dict(hh_search_field,hh_area,hh_period):
         pages_number = 1
         while page < pages_number:
             time.sleep(0.1)
-            page_response = requests.get(hh_url, params={'page': page,'text': language,'search_field': hh_search_field, 'area':int(hh_area),'period':int(hh_period)})
+            page_response = requests.get(hh_url, params={'page': page,'text': language,'search_field': name, 'area':1,
+            'period':30})
             page_response.raise_for_status()
             page_payload = page_response.json()
             pages_number = page_payload['pages']
             page += 1
-            hh_dict[language]["vacancies_found"] += len(page_payload['items'])
+            hh_job_data[language]["vacancies_found"] += len(page_payload['items'])
             for vacancy in page_payload["items"]:
                 salary = predict_rub_salary_hh(vacancy)
                 if salary is not None:
                     total_salary += salary
                     total_count += 1
         if total_count > 0:
-            hh_dict[language]["vacancies_processed"] = total_count
-            hh_dict[language]["average_salary"] = total_salary // total_count
-    return hh_dict
+            hh_job_data[language]["vacancies_processed"] = total_count
+            hh_job_data[language]["average_salary"] = total_salary // total_count
+    return hh_job_data
 
 
-def get_sj_dict(sj_token,sj_catalogue,sj_srws,sj_skws,sj_town):
+def get_sj_job_data(sj_token,sj_catalogue,sj_srws,sj_skws,sj_town):
     superjob_url = 'https://api.superjob.ru/2.0/vacancies/'
     headers = {
         'X-Api-App-Id': sj_token
@@ -83,7 +84,7 @@ def get_sj_dict(sj_token,sj_catalogue,sj_srws,sj_skws,sj_town):
     unixtime_one_month_ago = int(time.mktime(one_month_ago.timetuple()))
     
     languages = ['Python', 'Java', 'C++', 'JavaScript','C#','C','ruby','go', '1c', 'PHP','Shell', 'Scala', 'Swift']
-    superjob_dict = {lang: {"vacancies_found": 0, "vacancies_processed": 0, "average_salary": 0} for lang in languages}
+    sj_job_data = {lang: {"vacancies_found": 0, "vacancies_processed": 0, "average_salary": 0} for lang in languages}
     for language in languages:
         page = 0
         total_salary = 0
@@ -91,16 +92,16 @@ def get_sj_dict(sj_token,sj_catalogue,sj_srws,sj_skws,sj_town):
         while True:
             response = requests.get(superjob_url,  headers=headers,params={
                 'page': page,
-                'catalogues': int(sj_catalogue),
+                'catalogues': 48,
                 'date_published_from': unixtime_one_month_ago,
-                't':int(sj_town),
-                'srws': int(sj_srws),
-                'skwc': sj_skws,
+                't':4,
+                'srws': 1,
+                'skwc': AND,
                 'keys': language
             })
             response.raise_for_status()
             page_payload = response.json()
-            superjob_dict[language]["vacancies_found"] += len(page_payload['objects'])
+            sj_job_data[language]["vacancies_found"] += len(page_payload['objects'])
             for vacancy in page_payload['objects']:
                 salary = predict_rub_salary_sj(vacancy)
                 if salary is not None:
@@ -110,9 +111,9 @@ def get_sj_dict(sj_token,sj_catalogue,sj_srws,sj_skws,sj_town):
                 break
             page += 1
         if total_count > 0:
-            superjob_dict[language]["vacancies_processed"] = total_count
-            superjob_dict[language]["average_salary"] = total_salary // total_count
-    return superjob_dict
+            sj_job_data[language]["vacancies_processed"] = total_count
+            sj_job_data[language]["average_salary"] = total_salary // total_count
+    return sj_job_data
 
 
 def get_table(dict):
@@ -126,20 +127,20 @@ def get_table(dict):
 if __name__ == '__main__':
     load_dotenv()
     sj_token = os.environ["SJ_TOKEN"]
-    sj_catalogue = os.environ["SJ_CATALOGUES"]
-    sj_srws = os.environ["SJ_SRWS"]
-    sj_skws = os.environ["SJ_SKWS"]
-    sj_town = os.environ["SJ_TOWN"]
+#    sj_catalogue = os.environ["SJ_CATALOGUES"]
+#    sj_srws = os.environ["SJ_SRWS"]
+#    sj_skws = os.environ["SJ_SKWS"]
+#    sj_town = os.environ["SJ_TOWN"]
     
-    hh_search_field = os.environ["HH_SEARCH_FIELD"]
-    hh_area = os.environ["HH_AREA"]
-    hh_period = os.environ["HH_PERIOD"]
+#    hh_search_field = os.environ["HH_SEARCH_FIELD"]
+#    hh_area = os.environ["HH_AREA"]
+#    hh_period = os.environ["HH_PERIOD"]
     
-    hh_dict = get_hh_dict(hh_search_field,hh_area,hh_period)
-    hh_table = get_table(hh_dict)
+    hh_job_data = get_hh_job_data()
+    hh_table = get_table(hh_job_data)
     
-    sj_dict = get_sj_dict(sj_token,sj_catalogue,sj_srws,sj_skws,sj_town)
-    sj_table = get_table(sj_dict)
+    sj_job_data = get_sj_job_data(sj_token)
+    sj_table = get_table(sj_job_data)
     
     hh_tittle = 'HeadHunter Moscow'
     hh_table_instance = AsciiTable(hh_table,hh_tittle)
